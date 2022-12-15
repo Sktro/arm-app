029651d884e322f81511493aaf5c112430e59cce
Sktro <yissssss@yandex.ru>
2022-12-15 21:36:44 +0300

    Added definition of places by double events(fixed)

---
+++ src/components/HoldingATournament/TournamentsGrids/TournamentGridTo32/Grids/Result/PlacesForAthletes/PlaceSum/PlaceSum.tsx
@@ -0,0 +1,117 @@
+import React from "react";
+
+type PlaceSumType = {
+    countAthletes: number
+    arrAthletes: {
+        idAthletes: string,
+        athlete: string,
+        athleteWeight: number,
+        placeOnTheLeftHand: null | number,
+        placeOnTheRightHand: null | number,
+        placeForDoubleEvent: number,
+        pointsOnTheLeftHand: number,
+        pointsOnTheRightHand: number,
+        pointsSum: number
+    }[]
+}
+
+
+export const PlaceSum = (props: PlaceSumType) => {
+// добавить условие, если один из спортсменов не боролся на одной руке
+    // добавить условие, преимущество отдается тому, кто занял на одной из рук более высокое место
+    const athletesArrayCopy = props.arrAthletes.map(a => a)
+    let flagEqualWeights: boolean = false
+
+    for (let j = 0; j < props.countAthletes; j++) {
+        for (let i = 0; i < props.countAthletes - 1; i++) {
+            if (athletesArrayCopy[i].pointsSum < athletesArrayCopy[i + 1].pointsSum) {
+                let rt = athletesArrayCopy[i]
+                athletesArrayCopy[i] = athletesArrayCopy[i + 1]
+                athletesArrayCopy[i + 1] = rt
+                athletesArrayCopy.map((a,index) => a.placeForDoubleEvent = index+1)
+            }
+            if (athletesArrayCopy[i].pointsSum !== 0 && athletesArrayCopy[i + 1].pointsSum !== 0 && athletesArrayCopy[i].pointsSum === athletesArrayCopy[i + 1].pointsSum
+                && (athletesArrayCopy[i].placeOnTheLeftHand === athletesArrayCopy[i + 1].placeOnTheRightHand && athletesArrayCopy[i].placeOnTheRightHand === athletesArrayCopy[i + 1].placeOnTheLeftHand) &&
+                athletesArrayCopy[i].athleteWeight > athletesArrayCopy[i + 1].athleteWeight) {
+                console.log('1')
+                let rt = athletesArrayCopy[i]
+                athletesArrayCopy[i] = athletesArrayCopy[i + 1]
+                athletesArrayCopy[i + 1] = rt
+                athletesArrayCopy.map((a,index) => a.placeForDoubleEvent = index+1)
+            }
+            if (athletesArrayCopy[i].pointsSum !== 0 && athletesArrayCopy[i + 1].pointsSum !== 0 && athletesArrayCopy[i].pointsSum === athletesArrayCopy[i + 1].pointsSum
+                && (athletesArrayCopy[i].placeOnTheLeftHand === athletesArrayCopy[i + 1].placeOnTheRightHand && athletesArrayCopy[i].placeOnTheRightHand === athletesArrayCopy[i + 1].placeOnTheLeftHand) &&
+                athletesArrayCopy[i].athleteWeight === athletesArrayCopy[i + 1].athleteWeight) {
+                console.log('2')
+                flagEqualWeights = true
+            }
+            if(athletesArrayCopy[i].pointsSum !== 0 && athletesArrayCopy[i + 1].pointsSum !== 0 && athletesArrayCopy[i].pointsSum === athletesArrayCopy[i + 1].pointsSum
+                && ((athletesArrayCopy[i].placeOnTheLeftHand === athletesArrayCopy[i+1].placeOnTheRightHand && athletesArrayCopy[i].placeOnTheRightHand !== athletesArrayCopy[i+1].placeOnTheLeftHand)
+                    || (athletesArrayCopy[i+1].placeOnTheLeftHand === athletesArrayCopy[i].placeOnTheRightHand && athletesArrayCopy[i+1].placeOnTheRightHand !== athletesArrayCopy[i].placeOnTheLeftHand))
+                && ((athletesArrayCopy[i].placeOnTheLeftHand! + athletesArrayCopy[i].placeOnTheRightHand!) > (athletesArrayCopy[i+1].placeOnTheLeftHand! + athletesArrayCopy[i+1].placeOnTheRightHand!))){
+                console.log('3')
+                let rt = athletesArrayCopy[i]
+                athletesArrayCopy[i] = athletesArrayCopy[i + 1]
+                athletesArrayCopy[i + 1] = rt
+                athletesArrayCopy.map((a,index) => a.placeForDoubleEvent = index+1)
+            }
+            if(athletesArrayCopy[i].pointsSum !== 0 && athletesArrayCopy[i + 1].pointsSum !== 0 && athletesArrayCopy[i].pointsSum === athletesArrayCopy[i + 1].pointsSum
+                && (((athletesArrayCopy[i+1].placeOnTheLeftHand! < athletesArrayCopy[i].placeOnTheLeftHand!) && (athletesArrayCopy[i+1].placeOnTheLeftHand! < athletesArrayCopy[i].placeOnTheRightHand!))
+                    || ((athletesArrayCopy[i+1].placeOnTheRightHand! < athletesArrayCopy[i].placeOnTheLeftHand!) && (athletesArrayCopy[i+1].placeOnTheRightHand! < athletesArrayCopy[i].placeOnTheRightHand!)))){
+                console.log('4')
+                let rt = athletesArrayCopy[i]
+                athletesArrayCopy[i] = athletesArrayCopy[i + 1]
+                athletesArrayCopy[i + 1] = rt
+                athletesArrayCopy.map((a,index) => a.placeForDoubleEvent = index+1)
+            }
+                if(athletesArrayCopy[i].pointsSum === 0 && athletesArrayCopy[i+1].pointsSum === 0) {
+                    if((athletesArrayCopy[i].placeOnTheLeftHand! + athletesArrayCopy[i].placeOnTheRightHand!) === (athletesArrayCopy[i+1].placeOnTheLeftHand! + athletesArrayCopy[i+1].placeOnTheRightHand!)) {
+                        if((athletesArrayCopy[i].placeOnTheLeftHand === athletesArrayCopy[i + 1].placeOnTheRightHand && athletesArrayCopy[i].placeOnTheRightHand === athletesArrayCopy[i + 1].placeOnTheLeftHand) &&
+                            athletesArrayCopy[i].athleteWeight > athletesArrayCopy[i + 1].athleteWeight){
+                            console.log('5')
+                            let rt = athletesArrayCopy[i]
+                            athletesArrayCopy[i] = athletesArrayCopy[i + 1]
+                            athletesArrayCopy[i + 1] = rt
+                            athletesArrayCopy.map((a,index) => a.placeForDoubleEvent = index+1)
+                        }
+                        if((((athletesArrayCopy[i+1].placeOnTheLeftHand! < athletesArrayCopy[i].placeOnTheLeftHand!) && (athletesArrayCopy[i+1].placeOnTheLeftHand! < athletesArrayCopy[i].placeOnTheRightHand!))
+                            || ((athletesArrayCopy[i+1].placeOnTheRightHand! < athletesArrayCopy[i].placeOnTheLeftHand!) && (athletesArrayCopy[i+1].placeOnTheRightHand! < athletesArrayCopy[i].placeOnTheRightHand!)))){
+                            console.log('6')
+                            let rt = athletesArrayCopy[i]
+                            athletesArrayCopy[i] = athletesArrayCopy[i + 1]
+                            athletesArrayCopy[i + 1] = rt
+                            athletesArrayCopy.map((a,index) => a.placeForDoubleEvent = index+1)
+                        }
+                    } else {
+                        if (((athletesArrayCopy[i].placeOnTheLeftHand === athletesArrayCopy[i+1].placeOnTheRightHand && athletesArrayCopy[i].placeOnTheRightHand !== athletesArrayCopy[i+1].placeOnTheLeftHand)
+                                || (athletesArrayCopy[i+1].placeOnTheLeftHand === athletesArrayCopy[i].placeOnTheRightHand && athletesArrayCopy[i+1].placeOnTheRightHand !== athletesArrayCopy[i].placeOnTheLeftHand))
+                            && ((athletesArrayCopy[i].placeOnTheLeftHand! + athletesArrayCopy[i].placeOnTheRightHand!) > (athletesArrayCopy[i+1].placeOnTheLeftHand! + athletesArrayCopy[i+1].placeOnTheRightHand!))){
+                            console.log('7')
+                            let rt = athletesArrayCopy[i]
+                            athletesArrayCopy[i] = athletesArrayCopy[i + 1]
+                            athletesArrayCopy[i + 1] = rt
+                            athletesArrayCopy.map((a,index) => a.placeForDoubleEvent = index+1)
+                        }
+                        if((((athletesArrayCopy[i+1].placeOnTheLeftHand! < athletesArrayCopy[i].placeOnTheLeftHand!) && (athletesArrayCopy[i+1].placeOnTheLeftHand! < athletesArrayCopy[i].placeOnTheRightHand!))
+                            || ((athletesArrayCopy[i+1].placeOnTheRightHand! < athletesArrayCopy[i].placeOnTheLeftHand!) && (athletesArrayCopy[i+1].placeOnTheRightHand! < athletesArrayCopy[i].placeOnTheRightHand!)))){
+                            console.log('8')
+                            let rt = athletesArrayCopy[i]
+                            athletesArrayCopy[i] = athletesArrayCopy[i + 1]
+                            athletesArrayCopy[i + 1] = rt
+                            athletesArrayCopy.map((a,index) => a.placeForDoubleEvent = index+1)
+                        }
+                    }
+                    // if() // Ноль очков у обоих, каждый не участвовал на одной из рук, (места по "сумме" мест)
+                    // if() // Ноль очков у обоих, каждый не участвовал на одной из рук (места по весу)
+                }
+        }
+    }
+
+
+    return (
+        <div>
+            <div>Места по двоеборью</div>
+            {athletesArrayCopy.map(a => <div key={a.idAthletes}>{a.placeForDoubleEvent} место - {a.athlete} ({a.pointsSum})</div>)}
+        </div>
+    )
+}
